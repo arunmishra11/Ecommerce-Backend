b791880ba36d1e6fc8f26615786dd4825d428765
arunmishra305@gmail.com <arunmishra305@gmail.com>
2024-06-03 13:57:35 -0500

    updated category,tag,product

--- models/index.js
+++ models/index.js
@@ -1,16 +1,34 @@
-// import models
+// Require models.
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category.
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Categories have many Products.
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag).
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: {
+    model: ProductTag, 
+  }
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag).
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag, 
+  }
+})
 
 module.exports = {
   Product,
